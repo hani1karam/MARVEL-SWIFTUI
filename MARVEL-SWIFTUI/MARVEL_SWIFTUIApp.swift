dc6e3567bc9f80b77f9d1916aab2dfee61e4c598
hani1karam <hani1karam>
2024-03-01 20:35:48 +0200

    Initial Commit

---
+++ MARVEL-SWIFTUI/MARVEL_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MARVEL_SWIFTUIApp.swift
+//  MARVEL-SWIFTUI
+//
+//  Created by Mohamed Jaber on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MARVEL_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
